cfdb966018ab9dc555e414eb8e4da4d042a166d7
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-10 16:59:28 +0530

    Create Don-programe-2.js

---
+++ Don-programe-2.js
@@ -0,0 +1,28 @@
+Number1 = input ("Enter the first number ?")
+Number2 = input ("Enter the second number ?")
+
+def add(x, y):
+  return x + y
+ 
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
+
+def greater_than (x,y):
+    return x < y
+
+def less_than (x,y):
+    return x > y
+
+def is_equal (x,y):
+    return x!=y
+
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
