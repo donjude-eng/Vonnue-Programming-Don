6bfd82777c2ce94a927ca64313388e5e33a8c9ae
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-12 18:55:10 +0530

    Update Don-programe-2.js

--- Don-programe-2.js
+++ Don-programe-2.js
@@ -1,23 +1,20 @@
-function add(a, b) {
-    let sum = a + b;
+const add = (a , b) =>{
+    let add = a + b;
     console.log("The sum is", sum);
 }
 
-function substract(b,c) {
+const substract = (b,c) =>{
     let diff = b - c;
     console.log("The difference is", diff);
 }
-
-function multiply(d,e) {
+const multiply = (d,e) =>{
     let product = d * e;
     console.log("The product is", product);
 }
-
-function devide(f,g) {
+const devide = (f ,g ) =>{
     let quotient = f / g;
     console.log("The quotient is", quotient);
 }
-
 
 const num1 = prompt("Enter the first number");
 const num2 = prompt("Enter the second number");
